99e24d24544e8723f1f24842c3d20a72291a6b4c
Kenneth Løvgren Thomsen <kenneth.l.thomsen@gmail.com>
2021-12-01 21:55:08 +0100

    Cors failed

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ module.exports = {
     return [
       {
         source: '/api/:path*',
-        destination: 'https://api.example.com/:path*',
+        destination: 'https://api.foursquare.com/:path*',
       },
     ]
   },
