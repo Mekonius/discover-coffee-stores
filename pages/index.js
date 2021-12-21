6bcc5f498a1a7ffbda6ed94b6060232e403b3a76
Kenneth Løvgren Thomsen <kenneth.l.thomsen@gmail.com>
2021-12-21 13:31:09 +0100

    test this

--- pages/index.js
+++ pages/index.js
@@ -89,7 +89,7 @@ export default function Home(props) {
                     name={coffeeStore.name}
                     imgUrl={
                       coffeeStore.imgUrl ||
-                      "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
+                      'https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80'
                     }
                     href={`/coffee-store/${coffeeStore.id}`}
                     className={styles.card}
@@ -102,7 +102,7 @@ export default function Home(props) {
 
         {props.coffeeStores.length > 0 && (
           <>
-            <h2 className={styles.heading2}>London&#39;s coffee Stores</h2>
+            <h2 className={styles.heading2}>Londons coffee Stores</h2>
             <div className={styles.cardLayout}>
               {props.coffeeStores.map((coffeeStore) => {
                 return (
